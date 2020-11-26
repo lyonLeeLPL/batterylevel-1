1b60463b85328900852129919bc2bc6cdfe443b2
li.pinliang <lipinliang@intasect.co.jp>
2020-11-26 15:31:28 +0900

    local init v2

--- android/app/src/main/java/com/example/batterylevel/MainActivity.java
+++ android/app/src/main/java/com/example/batterylevel/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.batterylevel;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import io.dcloud.feature.sdk.DCUniMPJSCallback;
+import io.dcloud.feature.sdk.DCUniMPSDK;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
+
+import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -11,20 +17,42 @@ import android.os.BatteryManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.unimpdemo.MySplashView;
+
+import org.json.JSONObject;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "samples.flutter.dev/battery";
+    Context mContext;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+    }
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
+
+
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
                 .setMethodCallHandler(
                         (call, result) -> {
                             // Note: this method is invoked on the main thread.
                             if (call.method.equals("getBatteryLevel")) {
+                                try {
+                                    DCUniMPSDK.getInstance().startApp(mContext,"__UNI__04E3A11", MySplashView.class);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
                                 int batteryLevel = getBatteryLevel();
-
                                 if (batteryLevel != -1) {
                                     result.success(batteryLevel);
                                 } else {
